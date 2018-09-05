7ac9786bf2afb3b26deb0ef40914f64552b230b3
Sasha <alxblag@ya.ru>
2018-09-06 02:34:23 +0300

    Гистограмма-2
    1. Добавил облако и тень
    2. Добавил 4 гистограммы
    3. По мере возможности оптимизировал расчеты расположения гистограмм.
    
    NB: Не все численные значение удалось заменить на переменные, как это показано в 'Демке'. С чем это может быть связано ?

--- js/stat.js
+++ js/stat.js
@@ -10,17 +10,17 @@ var BARCHART_HEIGHT = 100;
 var TEXT_Y = 260;
 var GAP = 90;
 
-function renderCloud(ctx, x, y, color){
+function renderCloud (ctx, x, y, color){
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-};
+}
 
 window.renderStatistics = function (ctx, names, times) {
-  //Добавляем облако и его тень
+  //  Добавляем облако и его тень
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  //Добовляем заголовок для облако
+  //  Добовляем заголовок для облако
   ctx.fillStyle = '#000';
   ctx.font = ' bold 16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
@@ -32,26 +32,24 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'red';
   ctx.fillRect(BARCHART_X + BARCHART_X * 0, BARCHART_Y, BARCHART_WIDTH, BARCHART_HEIGHT);
 
-  //Гистограмма 2-ого участника
+  //  Гистограмма 2-ого участника
   ctx.fillStyle = '#000';
   ctx.fillText('Кекс', BARCHART_X + GAP * 1, TEXT_Y);
   ctx.fillStyle = 'green';
   ctx.fillRect(BARCHART_X + GAP * 1, BARCHART_Y, BARCHART_WIDTH, BARCHART_HEIGHT);
 
-  //Гистограмма 3-ого участника
+  //  Гистограмма 3-ого участника
   ctx.fillStyle = '#000';
   ctx.fillText('Катя', BARCHART_X + GAP * 2, TEXT_Y);
   ctx.fillStyle = 'blue';
   ctx.fillRect(BARCHART_X + GAP * 2, BARCHART_Y, BARCHART_WIDTH, BARCHART_HEIGHT);
 
-  //Гистограмма 4-ого участника
+  //  Гистограмма 4-ого участника
   ctx.fillStyle = '#000';
   ctx.fillText('Игорь', BARCHART_X + GAP * 3, TEXT_Y);
   ctx.fillStyle = 'grey';
   ctx.fillRect(BARCHART_X + GAP * 3, BARCHART_Y, BARCHART_WIDTH, BARCHART_HEIGHT);
 
-console.log(names);
-console.log(times);
-};
+  };
 
 
