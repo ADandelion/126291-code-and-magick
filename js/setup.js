c3b5d31e81eab444a6dc235ce9a724e1cbc5ab3a
Sasha <alxblag@ya.ru>
2018-09-17 00:40:59 +0300

    Правки по код гайду

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,5 @@
 'use strict';
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита','Вашингтон'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топльницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
@@ -8,9 +8,6 @@ var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
 document.querySelector('.setup-similar').classList.remove('hidden');
-
-
-
 
 // Случайно выбираем значение из массива
 var randomElement = function (el) {
@@ -22,17 +19,17 @@ var wizards = [
   {
     name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_SURNAME),
     coatColor: randomElement(COAT_COLOR),
-    eyesColor:  randomElement(EYES_COLOR)
+    eyesColor: randomElement(EYES_COLOR)
   },
   {
     name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_SURNAME),
     coatColor: randomElement(COAT_COLOR),
-    eyesColor:  randomElement(EYES_COLOR)
+    eyesColor: randomElement(EYES_COLOR)
   },
   {
     name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_SURNAME),
     coatColor: randomElement(COAT_COLOR),
-    eyesColor:  randomElement(EYES_COLOR)
+    eyesColor: randomElement(EYES_COLOR)
   },
   {
     name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_SURNAME),
@@ -55,7 +52,7 @@ var renderWizards = function (wizard) {
  // В процессе создания функции
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizards(wizards[i]));
+fragment.appendChild(renderWizards(wizards[i]));
 }
 similarListElement.appendChild(fragment);
 
